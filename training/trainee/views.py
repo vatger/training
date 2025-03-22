6c9d9f531f9eec59e26afaa298c9aa4f099f2780
fsoest <felix@soest-net.de>
2025-03-22 13:11:59 +0100

    Comments on user profile

--- training/trainee/views.py
+++ training/trainee/views.py
@@ -3,10 +3,13 @@ from typing import Optional
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, HttpResponseRedirect, reverse, redirect
 
 from lists.models import Course
 from logs.models import Log
+from .forms import CommentForm
+
+from connect.views import mentor_groups
 
 
 def split_active_inactive(logs, courses, trainee):
@@ -43,6 +46,9 @@ def mentor_view(request, vatsim_id: int):
     courses = request.user.mentored_courses.all()
     if request.user.is_superuser:
         courses = Course.objects.all()
+
+    if not request.user.groups.filter(name__in=mentor_groups).exists():
+        return redirect("/")
     # Get all logs for the trainee that are in the courses
     logs = Log.objects.filter(trainee=trainee, course__in=courses).order_by(
         "-session_date"
@@ -51,6 +57,21 @@ def mentor_view(request, vatsim_id: int):
     courses = set(Course.objects.filter(log__in=logs))
     active, inactive = split_active_inactive(logs, courses, trainee)
 
+    comments = trainee.comments.all().order_by("-date_added")
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data["text"]
+            author = request.user
+            trainee.comments.create(text=text, author=author)
+            return HttpResponseRedirect(
+                reverse("trainee:mentor_view", args=[trainee.username])
+            )
+    else:
+        form = CommentForm()
+
     return render(
-        request, "trainee/home.html", {"active": active, "inactive": inactive}
+        request,
+        "trainee/mentor_view.html",
+        {"active": active, "inactive": inactive, "comments": comments, "form": form},
     )
