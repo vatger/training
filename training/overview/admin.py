5e8293f622faa878393a8966f0dba179ff7d672e
fsoest <46381886+fsoest@users.noreply.github.com>
2025-03-24 21:18:22 +0100

    Update admin.py

--- training/overview/admin.py
+++ training/overview/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin.models import LogEntry
 
-# Register your models here.
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ("action_time", "user", "content_type", "object_repr", "action_flag", "change_message")
+    list_filter = ("user", "action_flag")
+    search_fields = ("object_repr", "change_message")
+
+    def has_add_permission(self, request):
+        return False  # Prevent manual addition
