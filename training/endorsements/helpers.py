d95939de38c4fb548bac6e9f9f08de6991d1be9c
fsoest <felix@soest-net.de>
2025-03-27 00:01:38 +0100

    Remove eud header from helpers

--- training/endorsements/helpers.py
+++ training/endorsements/helpers.py
@@ -1,18 +1,13 @@
-import os
 import requests
-from cachetools import TTLCache, cached
+from cachetools import cached, TTLCache
 from dotenv import load_dotenv
+from training.eud_header import eud_header
 
 load_dotenv()
 
 
-# @cached(cache=TTLCache(maxsize=1024, ttl=60 * 10))
+@cached(cache=TTLCache(maxsize=1024, ttl=60 * 10))
 def get_tier1_endorsements():
-    eud_header = {
-        "X-API-KEY": os.getenv("CORE_API"),
-        "Accept": "application/json",
-        "User-Agent": "VATGER",
-    }
     endorsements = requests.get(
         "https://core.vateud.net/api/facility/endorsements/tier-1", headers=eud_header
     ).json()["data"]
