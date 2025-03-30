7eb91dfb55c0eebddd27c45a825456c4243757f6
fsoest <felix@soest-net.de>
2025-03-30 16:49:59 +0200

    Reformat

--- training/overview/admin.py
+++ training/overview/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.contrib.admin.models import LogEntry
 
+
 @admin.register(LogEntry)
 class LogEntryAdmin(admin.ModelAdmin):
-    list_display = ("action_time", "user", "content_type", "object_repr", "action_flag", "change_message")
+    list_display = (
+        "action_time",
+        "user",
+        "content_type",
+        "object_repr",
+        "action_flag",
+        "change_message",
+    )
     list_filter = ("user", "action_flag")
     search_fields = ("object_repr", "change_message")
 
