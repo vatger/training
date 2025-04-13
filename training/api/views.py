9cd3b616334ae66704e408eb50bd68b23793d8e4
fsoest <felix@soest-net.de>
2025-04-13 18:59:24 +0200

    Remove endorsements, visitor status and from roster when gdpr removal

--- training/api/views.py
+++ training/api/views.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from dotenv import load_dotenv
 import os
+import requests
+
+from training.eud_header import eud_header
+from endorsements.helpers import remove_roster_and_endorsements
 
 load_dotenv()
 
@@ -17,8 +21,15 @@ def user_delete_view(request, vatsim_id):
         if auth_header == f"Token {os.getenv('GDPR_KEY')}":
             try:
                 user = User.objects.get(username=vatsim_id)
+                remove_roster_and_endorsements(int(vatsim_id))
+                requests.delete(
+                    f"https://core.vateud.net/api/facility/visitors/{int(vatsim_id)}/delete",
+                    headers=eud_header,
+                )
                 user.delete()
-                return JsonResponse({"message": "User deleted successfully"}, status=200)
+                return JsonResponse(
+                    {"message": "User deleted successfully"}, status=200
+                )
             except User.DoesNotExist:
                 return JsonResponse({"error": "User not found"}, status=200)
         else:
