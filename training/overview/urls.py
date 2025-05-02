08ebc139eaf27a3045d3ab98b4fd2ecfedb1ff21
fsoest <felix@soest-net.de>
2025-05-03 00:29:36 +0200

    Add removal url

--- training/overview/urls.py
+++ training/overview/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . import views
+from . import views, solo
 
 
 app_name = "overview"
@@ -11,7 +11,8 @@ urlpatterns = [
     path(
         "remove/<int:trainee_id>/<int:course_id>/", views.remove_trainee, name="remove"
     ),
-    path("solo/<int:vatsim_id>/<int:course_id>/", views.add_solo, name="add_solo"),
+    path("solo/<int:vatsim_id>/<int:course_id>/", solo.add_solo, name="add_solo"),
+    path("solo/delete/<int:solo_id>/", solo.delete_solo, name="delete_solo"),
     path(
         "finish/<int:trainee_id>/<int:course_id>/", views.finish_course, name="finish"
     ),
