0bf51d36acdcedf9233bfb2fbb354fad144e0f46
fsoest <felix@soest-net.de>
2025-05-24 13:53:28 +0200

    Update collectstatic

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sleep 1
-cd training && python manage.py collectstatic --noinput
-nginx -g "daemon on;"
+cd /opt/training/training && python manage.py collectstatic --noinput
 
+nginx -g "daemon on;"
 crontab cron_schedule && crond
-cd training && gunicorn training.wsgi --bind 127.0.0.1:8017
+cd /opt/training && gunicorn training.wsgi --bind 127.0.0.1:8017
