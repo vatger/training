e279c18f149b4fce051265986d3ae45ace65de34
fsoest <felix@soest-net.de>
2025-05-24 14:08:58 +0200

    Update init sh

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ cd /opt/training/training && python manage.py collectstatic --noinput
 
 nginx -g "daemon on;"
 crontab /opt/training/cron_schedule && crond
-cd training && gunicorn training.wsgi --bind 127.0.0.1:8017
+cd /opt/training/training && gunicorn wsgi --bind 127.0.0.1:8017
