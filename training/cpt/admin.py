a0b99fc954579936c4c69f1aa9c984497b37b90e
fsoest <felix@soest-net.de>
2025-05-31 17:32:32 +0200

    Update Examiner Admin Page

--- training/cpt/admin.py
+++ training/cpt/admin.py
@@ -4,8 +4,15 @@ from .models import Examiner, ExaminerPosition, CPT
 
 
 class ExaminerAdmin(admin.ModelAdmin):
+    list_display = ("user_username",)
     search_fields = ("user__username", "callsign")
     filter_horizontal = ("positions",)
+    autocomplete_fields = ["user"]
+
+    def user_username(self, obj):
+        return obj.user.username
+
+    user_username.short_description = "Username"
 
 
 admin.site.register(Examiner, ExaminerAdmin)
