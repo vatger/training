8aad822e18f62cb554a382defb75a0f37ca34758
soest <felix.soest@vatger.de>
2025-10-16 14:55:59 +0200

    Correct API link

--- training/endorsements/management/commands/update_activity.py
+++ training/endorsements/management/commands/update_activity.py
@@ -70,7 +70,7 @@ def get_hours(endorsement: dict) -> float:
     start = dj_timezone.now() - dj_timezone.timedelta(days=180)
     start = start.strftime("%Y-%m-%d")
     api_url = (
-        lambda id, start: f"http://stats.vatsim-germany.org/api/atc/{id}/sessions/?start={start}"
+        lambda id, start: f"http://stats.vatsim-germany.org/api/atc/{id}/sessions/?start_date={start}"
     )
     try:
         response = requests.get(api_url(vatsim_id, start)).json()
