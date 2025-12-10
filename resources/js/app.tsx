4f42eead623663863f3bbafd695b5da11564104a
Ben Böckmann <boeckmannben@gmail.com>
2025-12-10 19:22:02 +0100

    default laravel title removed

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,7 +5,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import { route } from 'ziggy-js';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME;
 
 // Make route function globally available
 declare global {
