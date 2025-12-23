70c787e4ce8c5333fe07601d21e1f489b46a8f74
Ben Böckmann <boeckmannben@gmail.com>
2025-12-23 20:54:12 +0000

    endorsement removal scheduling updated

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -42,7 +42,7 @@ return Application::configure(basePath: dirname(__DIR__))
             ->runInBackground();
 
         $schedule->command('endorsements:remove')
-            ->dailyAt('05:00')
+            ->dailyAt('01:00')
             ->withoutOverlapping();
 
         $schedule->command('roster:check')
