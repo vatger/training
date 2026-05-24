ca3620442253c5eb2b1eeb32f4b7dd9f40e10243
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2026-05-24 14:00:36 +0200

    add logging

--- start.sh
+++ start.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
+#
+mkdir -p /var/www/html/storage/logs
+touch /var/www/html/storage/logs/laravel.log
+chown -r www-data:www-data /var/www/html/*
+
 # Cache configuration for production
 if [ "$APP_ENV" = "production" ]; then
     # Clear package discovery cache
     rm -f bootstrap/cache/packages.php
     rm -f bootstrap/cache/services.php
-    
+
     # Discover packages (only installed ones, no dev)
     php artisan package:discover --ansi
-    
+
     # Cache config
     php artisan config:cache
 fi
