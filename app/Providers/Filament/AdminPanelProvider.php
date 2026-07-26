2f1f4a6612b820fb5bb2179594e4b397fd35fcfe
Ben Böckmann <boeckmannben@gmail.com>
2026-07-26 19:48:48 +0200

    remove login requirement from admin panel configuration

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,7 +26,6 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login()
             ->defaultThemeMode(ThemeMode::System)
             ->brandName('VATGER Training System')
             ->colors([
