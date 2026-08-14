3a126cd8bfd05e8c184802191db24e409f75e2f3
Ben Böckmann <boeckmannben@gmail.com>
2026-08-14 17:24:37 +0200

    build: chart component fixed for new version

--- resources/js/components/ui/chart.tsx
+++ resources/js/components/ui/chart.tsx
@@ -2,11 +2,13 @@
 
 import * as React from "react"
 import * as RechartsPrimitive from "recharts"
+import type { TooltipValueType } from "recharts"
 
 import { cn } from "@/lib/utils"
 
 // Format: { THEME_NAME: CSS_SELECTOR }
 const THEMES = { light: "", dark: ".dark" } as const
+type TooltipNameType = number | string
 
 export type ChartConfig = {
 	[k in string]: {
@@ -125,7 +127,13 @@ function ChartTooltipContent({
 		indicator?: "line" | "dot" | "dashed"
 		nameKey?: string
 		labelKey?: string
-	}) {
+	} & Omit<
+		RechartsPrimitive.DefaultTooltipContentProps<
+			TooltipValueType,
+			TooltipNameType
+		>,
+		"accessibilityLayer"
+	>) {
 	const { config } = useChart()
 
 	const tooltipLabel = React.useMemo(() => {
@@ -184,7 +192,7 @@ function ChartTooltipContent({
 					.map((item, index) => {
 						const key = `${nameKey || item.name || item.dataKey || "value"}`
 						const itemConfig = getPayloadConfigFromPayload(config, item, key)
-						const indicatorColor = color || item.payload.fill || item.color
+						const indicatorColor = color || item.payload?.fill || item.color
 
 						return (
 							<div
@@ -192,7 +200,7 @@ function ChartTooltipContent({
 									"[&>svg]:text-muted-foreground flex w-full flex-wrap items-stretch gap-2 [&>svg]:h-2.5 [&>svg]:w-2.5",
 									indicator === "dot" && "items-center",
 								)}
-								key={item.dataKey}
+								key={index}
 							>
 								{formatter && item?.value !== undefined && item.name ? (
 									formatter(item.value, item.name, item, index, item.payload)
@@ -234,9 +242,11 @@ function ChartTooltipContent({
 													{itemConfig?.label || item.name}
 												</span>
 											</div>
-											{item.value && (
+											{item.value != null && (
 												<span className="text-foreground font-mono font-medium tabular-nums">
-													{item.value.toLocaleString()}
+													{typeof item.value === "number"
+														? item.value.toLocaleString()
+														: String(item.value)}
 												</span>
 											)}
 										</div>
@@ -258,11 +268,10 @@ function ChartLegendContent({
 	payload,
 	verticalAlign = "bottom",
 	nameKey,
-}: React.ComponentProps<"div"> &
-	Pick<RechartsPrimitive.LegendProps, "payload" | "verticalAlign"> & {
-		hideIcon?: boolean
-		nameKey?: string
-	}) {
+}: React.ComponentProps<"div"> & {
+	hideIcon?: boolean
+	nameKey?: string
+} & RechartsPrimitive.DefaultLegendContentProps) {
 	const { config } = useChart()
 
 	if (!payload?.length) {
